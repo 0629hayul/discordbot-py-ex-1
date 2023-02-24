2845bd6655deed29894b5b3f88157c337d971c46
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-02-24 11:42:08 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -29,12 +29,9 @@ async def on_message(message):
         await message.send('{0.author.mention} Hello!'.format(message))
 
     if message.content == f'{PREFIX}help':
-	embedVar = discord.Embed(title="telum-명령어", description="* 접두사:`(따옴표X)")
-	embedVar.add_field(value="call : 봇 실행 여부 확인", inline=False)
-	embedVar.add_field(value="`help : 기본 명령어 확인", inline=False)
-	embedVar.add_field(value="`hi : 인사", inline=False)
-	embedVar.add_field(value="`hello : 멘션 기능 구현중", inline=False)
-
+        embedVar = discord.Embed(title="Title", description="Desc", color=0x00ff00)
+        embedVar.add_field(name="Field1", value="hi", inline=False)
+        embedVar.add_field(name="Field2", value="hi2", inline=False)
         await message.channel.send(embed=embedVar)
         
     if (command === 'avatar') {
