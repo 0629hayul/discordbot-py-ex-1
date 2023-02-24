f555388c7fe8a938f5b24b354f45cd044d0644c8
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-02-24 10:34:57 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -22,11 +22,6 @@ async def on_message(message):
 
     if message.content == f'{PREFIX}call':
         await message.channel.send("callback!")
-        
-    if message.content == f'{PREFIX}embed':
-    embed=discord.Embed(title="embed messege test", description="hello world")
-    embed.add_field(name="undefined", value="undefined", inline=False)
-    await ctx.send(embed=embed)
 
     if message.content.startswith(f'{PREFIX}hello'):
       @commands.command(pass_context=True)
