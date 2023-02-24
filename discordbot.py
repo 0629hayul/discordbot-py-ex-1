5310c2843e82c9e573d827f2e6d9784994fe050b
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-02-24 11:35:12 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,19 +19,38 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
-    if message.content == f'{PREFIX}call':
+    if message.content.startswith(f'{PREFIX}hi'):
+        await message.channel.send("hi!")
+        
+    if message.content == f'call':
         await message.channel.send("callback!")
 
     if message.content.startswith(f'{PREFIX}hello'):
         await message.send('{0.author.mention} Hello!'.format(message))
 
-    if message.content.startswith('em'):
-        embedVar = discord.Embed(title="Title", description="Desc", color=0x00ff00)
-        embedVar.add_field(name="Field1", value="hi", inline=False)
-        embedVar.add_field(name="Field2", value="hi2", inline=False)
+    if message.content == f'{PREFIX}help':
+            embedVar = discord.Embed(title="telum-명령어", description="* 접두사:`(따옴표X)")
+        embedVar.add_field(value="call : 봇 실행 여부 확인", inline=False)
+        embedVar.add_field(value="`help : 기본 명령어 확인", inline=False)
+        embedVar.add_field(value="`hi : 인사", inline=False)
+        embedVar.add_field(value="`hello : 멘션 기능 구현중", inline=False)
+
         await message.channel.send(embed=embedVar)
-    
+        
+    if (command === 'avatar') {
+		if (args[0]) {
+			const user = getUserFromMention(args[0]);
+			if (!user) {
+				return message.reply('Please use a proper mention if you want to see someone elses avatar.');
+			}
+
+			return message.channel.send(`${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`);
+		}
+
+		return message.channel.send(`${message.author.username}, your avatar: ${message.author.displayAvatarURL({ dynamic: true })}`);
+	}
+});
+            
 try:
     client.run(TOKEN)
 except discord.errors.LoginFailure as e:
