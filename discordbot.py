ee80f46d0f33735cd15de6faaf919d0104329622
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-02-24 13:36:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,20 +19,20 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith(f'{PREFIX}hi'):
-        await message.channel.send("hi!")
         
     if message.content == f'call':
         await message.channel.send("callback!")
+    
+    if message.content == f'test':
+        await message.channel.send('{#1077856697525219381}')
 
     if message.content.startswith(f'{PREFIX}hello'):
         await message.channel.send('hello, {0.author.mention}!'.format(message))
 
     if message.content == f'{PREFIX}help':
         embedVar = discord.Embed(title="Telum_bot", color=0x3B3B3B)
-        embedVar.add_field(name="*접두사 :ㅤ.",value="- .help : telum 봇 명령어 확인", inline=False)
-        embedVar.add_field(name="",value="- .hi : 인사", inline=False)
-        embedVar.add_field(name="",value="- .hello : 멘션 기능 구현중", inline=False)
+        embedVar.add_field(name="*접두사 :ㅤ.",value="- help : telum 봇 명령어 확인", inline=False)
+        embedVar.add_field(name="",value="- hello : 인사", inline=False)
         embedVar.add_field(name="",value="- call : (접두사X)봇 확인", inline=False)
         await message.channel.send(embed=embedVar)
         
