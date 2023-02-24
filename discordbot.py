bdf6c9fda2327ad819d4e240f0cec3e406814dca
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-02-24 10:55:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,7 @@ from cmath import log
 from distutils.sysconfig import PREFIX
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 import os
 load_dotenv()
 
@@ -23,10 +24,10 @@ async def on_message(message):
         await message.channel.send("callback!")
 
     if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello!')
+        await ctx.send('{0.author.mention} Hello!'.format(ctx))
 
     if message.content == f'{PREFIX}embed':
-        embed = discord.Embed(title="title", description='description', color=discord.Color.green())
+        embed=discord.Embed(title="Sample Embed", url="https://realdrewdata.medium.com/", description="This is an embed that will show how to build an embed and the different components")
         await ctx.send(embed=embed)
     
 try:
