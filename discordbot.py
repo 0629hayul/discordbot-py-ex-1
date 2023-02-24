27909588d24d8d18240f954d4e1b7c1274d1663f
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-02-24 16:16:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -36,6 +36,42 @@ async def on_message(message):
         embedVar.add_field(name="",value="- call : (접두사X)봇 확인", inline=False)
         await message.channel.send(embed=embedVar)
         
+        newUserMessage = """
+You
+can
+put
+your
+multiline
+message
+here!
+"""
+
+@client.event
+async def on_member_join(member):
+    print("Recognised that a member called " + member.name + " joined")
+    try: 
+        await client.send_message(member, newUserMessage)
+        print("Sent message to " + member.name)
+    except:
+        print("Couldn't message " + member.name)
+    embed=discord.Embed(
+        title="Welcome "+member.name+"!"
+        description="We're so glad you're here!"
+        color=discord.Color.green()
+    )
+        
+    role = discord.utils.get(member.server.roles, name="name-of-your-role") #  Gets the member role as a `role` object
+    await client.add_roles(member, role) # Gives the role to the user
+    print("Added role '" + role.name + "' to " + member.name)
+
+@client.event
+async def on_member_leave(member):
+    print("Recognised that a member called " + member.name + " left")
+    embed=discord.Embed(
+        title="😢 Goodbye "+member.name+"!",
+        description="Until we meet again old friend." # A description isn't necessary, you can delete this line if you don't want a description.
+        color=discord.Color.red() # There are lots of colors, you can check them here: https://discordpy.readthedocs.io/en/latest/api.html?highlight=discord%20color#discord.Colour
+    )
             
 try:
     client.run(TOKEN)
