73d3df26642c0b6043dc23eaf50751127bd8114c
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-03-02 20:56:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -24,7 +24,8 @@ async def on_message(message):
     if message.content == f'{PREFIX}dm':
         await message.channel.send('입력하셈')
         message = await client.wait_for('message')
-        await message.channel.send(message.content)
+        channel = bot.get_channel(1080820800149524530)
+        await channel.send(message.content)
    
     if message.content == f'{PREFIX}멤버등록':
         await message.channel.send('멤버등록은 <#1077925680903376896>을 참고해주세요!')
