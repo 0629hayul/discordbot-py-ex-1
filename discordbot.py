04a652faacce1ca6626cf6e3ce4abb2e450cd37c
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-03-03 22:35:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -25,8 +25,7 @@ async def on_message(message):
         await message.channel.send('멤버등록은 <#1077925680903376896>을 참고해주세요!')
    
     if message.content == f'{PREFIX}tb':
-        mention = format(message.author.display_name)
-        await message.channel.send('<@819436785998102548> 잠시만 기다려주세요 {mention}님, 대기열 확인 후 게임시작합니다.')
+        await message.channel.send('<@819436785998102548> 잠시만 기다려주세요 {message.author.mention} 님, 대기열 확인 후 게임시작합니다.')
   
 
     if message.content == f'{PREFIX}게임':
